de0b6610dbdee20069f13233d7f7882d0520e051
Peter Chay <peterchay88@gmail.com>
2023-07-21 14:57:19 -0600

    Import nltk and creating the analyzer object

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+# Download the vader_lexicon resource if not already present & assign the analyzer object
+nltk.download('vader_lexicon')
+analyzer = SentimentIntensityAnalyzer()
 
 st.title("Diary Tone")
 
