a4b3e9b038ba97bb7541d768a30d2890e65d68ed
Peter Chay <peterchay88@gmail.com>
2023-07-21 22:09:12 -0600

    Script now is able to return all the sentiment scores and dates from the text files

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 import os
+import re
 
 # Download the vader_lexicon resource if not already present & assign the analyzer object
 nltk.download('vader_lexicon')
@@ -13,19 +14,29 @@ def read_text_file(file_path):
     with open(file_path, 'r') as f:
         slash_index = file_path.find("/")
         txt_name = file_path[slash_index +1:]
-        read_text = f"{txt_name}\n{f.read()}"
+        read_text = f"{txt_name}\n{f.read()}\n"
     return read_text
 
 
 diary_text = ""
 for file in os.listdir("diary/"):
-    try:
-        if file.endswith(".txt"):
-            file_path = f"diary/{file}"
-            diary_text += read_text_file(file_path)
-    except FileNotFoundError:
-        print("could not read")
+    if file.endswith(".txt"):
+        file_path = f"diary/{file}"
+        diary_text += read_text_file(file_path)
 
+# Using Regular Expression to list dates from the title of the text files
+pattern = re.compile("2023-10-[0-9]+")
+dates = re.findall(pattern, diary_text)
+
+# creating a list of scores from the text files
+list_of_scores = []
+for file in os.listdir("diary/"):
+    if file.endswith(".txt"):
+        file_path = f"diary/{file}"
+        diary_text = read_text_file(file_path)
+        list_of_scores.append(analyzer.polarity_scores(diary_text))
+
+# Merge the list of scores and the list of dates into a list of tuples
+merged_list = [(dates[i], list_of_scores[i]) for i in range(0, len(dates))]
 
 st.title("Diary Tone")
-st.text(diary_text)
